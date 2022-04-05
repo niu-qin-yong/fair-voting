bfcc2b85a64995305e263292fbe948b8a40a5f07
niuqy <nqyimtheonlyone@163.com>
2022-04-06 07:10:14 +0800

    complete deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,6 @@
-// This is a script for deploying your contracts. You can adapt it to deploy
-// yours, or create new ones.
+
+const { ethers } = require("hardhat");
+
 async function main() {
   // This is just a convenience check
   if (network.name === "hardhat") {
@@ -19,9 +20,15 @@ async function main() {
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  await deploy("VoteCoin");
-  await deploy("VoteGift");
-  // deploy("Voting",["Satoshi","Vitalik"],)
+  // deploy
+  let voteCoin = await deploy("VoteCoin");
+  let voteGift = await deploy("VoteGift");
+  let voting = await deploy("Voting",["Satoshi","Vitalik"],voteCoin.address,voteGift.address,ethers.utils.parseEther("0.001"));
+  
+  // initialize
+  voteCoin.instance.mint(voting.address,10000);
+  voteGift.instance.transferOwnership(voting.address);
+
 }
 
 async function deploy(contractName,...args){
@@ -29,10 +36,9 @@ async function deploy(contractName,...args){
   const token = await Token.deploy(...args);
   await token.deployed();
 
-  console.log(contractName + " address:", token.address);
+  console.log(contractName + " deployed address:", token.address);
 
-  // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(token);
+  return {"instance":token,"address":token.address};
 }
 
 function saveFrontendFiles(token) {
